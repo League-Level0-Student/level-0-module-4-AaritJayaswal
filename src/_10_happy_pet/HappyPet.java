0f8d19e1edd7e47675160a5157f3fcf2e641da8a
League Student <league@iMac-12.attlocal.net>
2020-02-08 12:51:04 -0800

    Update HappyPet.java

--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -5,17 +5,16 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
-		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
+String pet= JOptionPane.showInputDialog(null,"What kind of pet do you want?");
+		// 7. REPEAT steps 3 - 6 enough times to make your.
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "Make your"+ pet+" happy by", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { " giving food", "take a walk", "giving water" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
 
@@ -28,3 +27,30 @@ public class HappyPet {
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
 }
+
+/*
+               ????????????
+              ??          ??
+             ??            ??
+			??              ??
+					        ?? 
+					        ??
+					        ??
+					       ??
+					      ??
+					     ??
+					    ??
+					   ??
+					   ??
+					   ??
+					   ??
+					   ??
+							   
+					   ?? 		   
+							   
+							   
+							   
+							   
+*/				   
+							   
+							   
